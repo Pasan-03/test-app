d8cda208e83833f8fea42f3d30af89fb0446a20a
Pasan-03 <donachinthapasan1@gmail.com>
2024-09-20 15:40:03 +0530

    error fixed in aws

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        MONGO_URI: process.env.MONGO_URI,
+    },
+    reactStrictMode: true, // Enables React's strict mode for better error checking
+    webpack: (config) => {
+        // Customize the webpack config here if needed
+        return config;
+    },
+};
 
 export default nextConfig;
