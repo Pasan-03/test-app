176253be3a07d077d2bb9cbe6887d71020c10792
Pasan-03 <donachinthapasan1@gmail.com>
2024-09-20 15:53:16 +0530

    again

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
+    console.error('MONGO_URI environment variable is not defined');
     throw new Error('Please define the MONGO_URI environment variable inside .env.local');
 }
 
