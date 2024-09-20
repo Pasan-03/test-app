09c85b43e474272d5d24019f153f672ac99ebd6b
Pasan-03 <donachinthapasan1@gmail.com>
2024-09-20 15:49:00 +0530

    another error fixed

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ const nextConfig = {
     env: {
         MONGO_URI: process.env.MONGO_URI,
     },
-    reactStrictMode: true, // Enables React's strict mode for better error checking
-    webpack: (config) => {
-        // Customize the webpack config here if needed
-        return config;
-    },
 };
 
 export default nextConfig;
